89eb807055f05b10ed311e9027824eaca4030cbe
Jeremy Polanco Lara <jeremyinf26@gmail.com>
2022-06-10 16:02:59 -0400

    addedfew fixes

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 
-app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static("./public"));
@@ -41,16 +40,6 @@ app.use(fileUpload());
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
-
-// routes
-app.get("/api/v1", (req, res) => {
-  // console.log(req.cookies);
-  console.log(req.signedCookies);
-  res.status(200).send("e-commerce API");
-});
-app.get("/", (req, res) => {
-  res.status(200).send("<h1>e-commerce API</h1>");
-});
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
